762df9dc58b01c6de3b84df180a6836853421cce
DrowsyWings <114069481+DrowsyWings@users.noreply.github.com>
2024-06-15 00:09:53 +0530

    try1

--- app/main.js
+++ app/main.js
@@ -6,11 +6,11 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const stringedData = data.toString().split(" ")[2];
-    if (stringedData !== "/index.html") {
-      const response = "HTTP/1.1 404 Not Found\r\n\r\n";
-      socket.write(response);
+    if (stringedData == "/index.html") {
+      const response1 = "HTTP/1.1 200 OK\r\n\r\n";
+      socket.write(response1);
     } else {
-      const response = "HTTP/1.1 200 OK\r\n\r\n";
+      const response = "HTTP/1.1 404 Not Found\r\n\r\n";
       socket.write(response);
     }
     socket.end();
