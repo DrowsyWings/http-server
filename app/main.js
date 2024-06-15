f89b0529e071476e68ec84be6674580aba7a9ba1
DrowsyWings <114069481+DrowsyWings@users.noreply.github.com>
2024-06-16 00:06:16 +0530

    try1

--- app/main.js
+++ app/main.js
@@ -5,11 +5,19 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const stringedData = data.toString().split(" ")[1];
-    console.log("********************" + stringedData);
-    if (stringedData == "/") {
-      const response1 = "HTTP/1.1 200 OK\r\n\r\n";
-      socket.write(response1);
+    const splitData = data.toString().split("/");
+    if (splitData == "/") {
+      const fileName = data.toString().split(" ")[1].split("/")[2];
+      if (fileName) {
+        const response1 =
+          "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n" +
+          fileName;
+        socket.write(response1);
+      } else {
+        const response2 =
+          "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n";
+        socket.write(response2);
+      }
     } else {
       const response = "HTTP/1.1 404 Not Found\r\n\r\n";
       socket.write(response);
